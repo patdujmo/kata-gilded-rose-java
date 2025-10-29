7f2a33438b70c3a4760d541fff14099fa1b61aa3
Casalisa123 <alisa.jung@cas.de>
2025-10-29 10:39:54 +0000

    More WIP

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -17,86 +17,28 @@ class GildedRose {
                 case "Sulfuras, Hand of Ragnaros" -> updateHand(items[i]);
                 default -> updateGeneralItem(items[i]);
             };
-            /*
-
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
-                */
         }
     }
 
     private Item updateAgedBried(Item item) {
-        if (item.quality < 50)
-            item.quality = item.quality + 1;
+        item.quality = increaseQualityBelowThreshold(item.quality);
         item.sellIn = item.sellIn - 1;
-        if (item.sellIn < 0 && item.quality < 50)
-            item.quality = item.quality + 1;
+        if (item.sellIn < 0) {
+            item.quality = increaseQualityBelowThreshold(item.quality);
+        }
 
         return item;
     }
 
     private Item updateBackstagePasses(Item item) {
-        if (item.quality < 50) {
-            item.quality = item.quality + 1;
+        item.quality = increaseQualityBelowThreshold(item.quality);
 
-            if (item.sellIn < 11) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            }
+        if (item.sellIn < 11) {
+            item.quality = increaseQualityBelowThreshold(item.quality);
+        }
 
-            if (item.sellIn < 6) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            }
-
+        if (item.sellIn < 6) {
+            item.quality = increaseQualityBelowThreshold(item.quality);
         }
 
         item.sellIn = item.sellIn - 1;
@@ -119,6 +61,13 @@ class GildedRose {
             item.quality = item.quality - 1;
         }
         return item;
+    }
+
+    private int increaseQualityBelowThreshold(int quality) {
+        if (quality < 50)
+            return quality + 1;
+        else
+            return quality;
     }
 
 }
